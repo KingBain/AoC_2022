23e8f89d7f068cb1c317650ab1ab66c6d63e0a05
Bainer <john.bain@gmail.com>
2022-12-06 09:32:19 -0500

    much easier

---
+++ Day6/Silver/answer.ps1
@@ -0,0 +1,22 @@
+$patternWidth = 4
+$buffer = Get-Content -Path "..\answer_data.txt"
+
+#$buffer -split "(.{$patternWidth})" -ne '' | foreach-object {
+#    "$_" | Select-Object -Unique
+#}
+
+$index = 0
+$buffer.ToCharArray() | ForEach-Object {
+    $slot = $($buffer[($index)..($index+($patternWidth-1))])
+    #"anaylzing $_ at position $index"
+    #"next $($patternWidth-1) packets $slot"
+    
+    $results = $slot | Group-Object | Where-Object {$_.count -gt 1}
+    
+    if (-not $results){
+        "found start of marker at $($index+$patternWidth)"
+        break
+    }
+
+$index ++
+}
