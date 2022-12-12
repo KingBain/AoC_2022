19ed99443731b165e3bc0b6f1bde108e6e1a8781
Bainer <john.bain@gmail.com>
2022-12-12 10:13:47 -0500

    🤜🤜🤜🐵🐵🐵😡😒😠😠

--- Day11/Gold/aswer2.ps1
+++ Day11/Gold/aswer2.ps1
@@ -1,5 +1,5 @@
-$monkeyBuilder = Get-Content -Path "..\dummy_data.txt" 
-#$monkeyBuilder = Get-Content -Path "..\answer_data.txt" 
+#$monkeyBuilder = Get-Content -Path "..\dummy_data.txt" 
+$monkeyBuilder = Get-Content -Path "..\answer_data.txt" 
 
 class Monkey{
     [object]$Items
@@ -15,25 +15,24 @@ class Monkey{
         ($this.Items).clone() | ForEach-Object {
 
             $this.InspectionCount++
-            [Uint64]$oppValue = ''
+            #write-host "big number $_"
+            $oppValue = ''
             if ($this.OperationValue -eq -1){
-                $oppValue = $_
+                #write-host "here"
+                $oppValue = [bigint]$_
+                
             } else {
                 $oppValue = $this.OperationValue
             }
             
             if ($this.OperationMulti) {
-                [Uint64]$new = [Uint64]$_ * [Uint64]$oppValue
+                [int64]$new = [int64]$_ * [int64]$oppValue
             } else {
-                [Uint64]$new = [Uint64]$_ + [Uint64]$oppValue
+                [int64]$new = [int64]$_ + [int64]$oppValue
             }
-            
-            #write-host ("New value is $new")
-            
-            #$new = [int]($new / 3)
-            #$new = [Math]::Floor([decimal]($new / 3))
-
-           # write-host ("New value is $new after divideby 3" )
+                  
+            #this is the magi right here, its the LCM for all the monkeys
+            [int64]$new = [int64]($new % $lcm)
 
 
             if ($new % $this.TestValue -eq 0){
@@ -49,6 +48,42 @@ class Monkey{
         $itemCounter++   
         }
     }
+}
+
+function lcm($String){
+    $array=@()
+    [System.Double]$product=1
+    $Numbers = $String.Split(",")
+    foreach ($Number in $Numbers) {
+        $sqrt=[math]::sqrt($number)
+        $Factor=2
+        $count=0
+        while ( ($Number % $Factor) -eq 0) {
+            $count+=1
+            $Number=$Number/$Factor
+            if (($array | Where-Object {$_ -eq $Factor}).count -lt $count) {
+                $array+=$Factor
+            }
+        }
+        $count=0
+        $Factor=3
+        while ($Factor -le $sqrt) {
+            while ( ($Number % $Factor) -eq 0) {
+                $count+=1
+                $Number=$Number/$Factor
+                if (($array | Where-Object {$_ -eq $Factor}).count -lt $count) {
+                    $array+=$Factor
+                    }
+                }           
+            $Factor+=2
+            $count=0
+        }
+        if ($array -notcontains $Number) {
+            $array+=$Number
+        }
+    }
+    foreach($arra in $array) {$product = $product * $arra}
+    return $product
 }
 
 $tree = New-Object System.Collections.ArrayList
@@ -82,7 +117,9 @@ $monkeyBuilder | ForEach-Object {
     }  
 }
 
-$rounds = 20
+$lcm = lcm -string ([string]$tree.TestValue -replace (' ',','))
+
+$rounds = 10000
 
 1..$rounds | ForEach-Object {
     "Round $_"
@@ -96,6 +133,5 @@ $rounds = 20
 }
 $score = $tree.InspectionCount | Sort-Object -Descending | Select-Object -First 2
 
-$score[0] * $score[1]
 
 "Total Monkey Business $($score[0] * $score[1])"
